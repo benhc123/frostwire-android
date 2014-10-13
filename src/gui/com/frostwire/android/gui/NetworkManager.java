2c9e02d59bcc77ba9860e60d699903bd455d013b
Alden Torres <aldenml@gmail.com>
2014-10-13 17:51:51 -0400

    No more WiMAX. Cleanup.

--- src/gui/com/frostwire/android/gui/NetworkManager.java
+++ src/gui/com/frostwire/android/gui/NetworkManager.java
@@ -18,76 +18,27 @@
 
 package com.frostwire.android.gui;
 
-import java.io.IOException;
-import java.lang.reflect.Field;
-import java.net.InetAddress;
-
 import android.app.Application;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.wifi.WifiManager;
-import android.util.Log;
-
 import com.frostwire.android.core.ConfigurationManager;
 import com.frostwire.android.core.Constants;
 import com.frostwire.android.core.CoreRuntimeException;
 import com.frostwire.util.ByteUtils;
 
+import java.io.IOException;
+import java.net.InetAddress;
+
 /**
  * @author gubatron
  * @author aldenml
- *
  */
 public final class NetworkManager {
-
-    private static final String TAG = "FW.NetworkManager";
-
-    private static Field inetAddressHostNameField;
-
-    // The following constants are from the android sdk source code
-    // It does not matter if some constants are from a newer android version,
-    // since only the latest phone can go to the specified state.
-
-    /**
-     * Current network is UMTS - aka GSM's 3G (AT&T)
-     */
-    private static final int NETWORK_TYPE_UMTS = 3;
-
-    /**
-     * Current network is EVDO revision B - aka CDMA's 3G (Verizon)
-     */
-    private static final int NETWORK_TYPE_EVDO_B = 12;
-
-    /**
-     * Verizon's 4G LTE (as seen on the Thunderbolt)
-     */
-    private static final int NETWORK_TYPE_4G_LTE = 13;
-
-    /**
-     * Verizon's 4G eHRPD 
-     * http://developer.motorola.com/docstools/library/detecting-and-using-lte-networks/
-     */
-    private static final int NETWORK_TYPE_4G_EHRPD = 14;
-
-    /**
-     * The Default WiMAX data connection.  When active, all data traffic
-     * will use this connection by default.  Should not coexist with other
-     * default connections.
-     */
-    private static final int TYPE_WIMAX = 6;
 
     private final Application context;
 
     private int listeningPort;
-
-    static {
-        try {
-            inetAddressHostNameField = InetAddress.class.getDeclaredField("hostName");
-            inetAddressHostNameField.setAccessible(true);
-        } catch (Throwable e) {
-            Log.e(TAG, "Error getting inetAddressHostNameField", e);
-        }
-    }
 
     private static NetworkManager instance;
 
@@ -121,37 +72,15 @@ public final class NetworkManager {
 
     public boolean isDataUp() {
         // boolean logic trick, since sometimes android reports WIFI and MOBILE up at the same time
-        return (isDataWIFIUp() != isDataMobileUp()) || isDataWiMAXUp();
+        return (isDataWIFIUp() != isDataMobileUp());
     }
 
     public boolean isDataMobileUp() {
-        ConnectivityManager connectivityManager = getConnectivityManager();
-        NetworkInfo info = connectivityManager.getActiveNetworkInfo();
-        return info != null && info.getType() == ConnectivityManager.TYPE_MOBILE && info.isAvailable() && info.isConnected();
-    }
-
-    public boolean isData3GUp() {
-        ConnectivityManager connectivityManager = getConnectivityManager();
-        NetworkInfo networkInfo = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
-        return networkInfo != null && networkInfo.isAvailable() && (networkInfo.getSubtype() == NETWORK_TYPE_UMTS || networkInfo.getSubtype() == NETWORK_TYPE_EVDO_B) && networkInfo.isConnected();
+        return isDataUp(ConnectivityManager.TYPE_MOBILE);
     }
 
     public boolean isDataWIFIUp() {
-        ConnectivityManager connectivityManager = getConnectivityManager();
-        NetworkInfo networkInfo = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
-        return networkInfo != null && networkInfo.isAvailable() && networkInfo.isConnected();
-    }
-
-    public boolean isDataWiMAXUp() {
-        ConnectivityManager connectivityManager = getConnectivityManager();
-        NetworkInfo networkInfo = connectivityManager.getNetworkInfo(TYPE_WIMAX);
-        return networkInfo != null && networkInfo.isAvailable() && networkInfo.isConnected();
-    }
-
-    public boolean isData4GUp() {
-        ConnectivityManager connectivityManager = getConnectivityManager();
-        NetworkInfo networkInfo = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
-        return networkInfo != null && networkInfo.isAvailable() && (networkInfo.getSubtype() == NETWORK_TYPE_4G_LTE || networkInfo.getSubtype() == NETWORK_TYPE_4G_EHRPD) && networkInfo.isConnected();
+        return isDataUp(ConnectivityManager.TYPE_WIFI);
     }
 
     public WifiManager getWifiManager() {
@@ -161,11 +90,13 @@ public final class NetworkManager {
     public InetAddress getMulticastInetAddress() throws IOException {
         WifiManager wifi = getWifiManager();
         int intaddr = wifi.getConnectionInfo().getIpAddress();
-        byte[] byteaddr = new byte[] { (byte) (intaddr & 0xff), (byte) (intaddr >> 8 & 0xff), (byte) (intaddr >> 16 & 0xff), (byte) (intaddr >> 24 & 0xff) };
+        byte[] byteaddr = new byte[]{(byte) (intaddr & 0xff), (byte) (intaddr >> 8 & 0xff), (byte) (intaddr >> 16 & 0xff), (byte) (intaddr >> 24 & 0xff)};
         return InetAddress.getByAddress(byteaddr);
     }
-    
-    private ConnectivityManager getConnectivityManager() {
-        return (ConnectivityManager) context.getSystemService(Application.CONNECTIVITY_SERVICE);
+
+    private boolean isDataUp(int type) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Application.CONNECTIVITY_SERVICE);
+        NetworkInfo info = connectivityManager.getActiveNetworkInfo();
+        return info != null && info.getType() == type && info.isAvailable() && info.isConnected();
     }
 }
